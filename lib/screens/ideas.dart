045cb84aa909819146d5fde3a2dc4abb2581183e
akhilbk123 <akhil.balakrishnan@outlook.com>
2021-05-11 18:30:48 +0530

    Version11_05
    Version11_05

--- lib/screens/ideas.dart
+++ lib/screens/ideas.dart
@@ -6,36 +6,61 @@ class Ideas extends StatefulWidget {
 }
 
 class _IdeasState extends State<Ideas> {
-  List<Widget> listofwidgets = [];
+  List<String> _text = ['Idea'];
+  TextEditingController _c;
+  List<int> listofwidgets = [];
+
+  var i = 0;
 
   void additemtolist() {
-    List<Widget> tempList = listofwidgets;
-    tempList.add(Text("Iam Added"));
+    List<int> tempList = listofwidgets;
+    tempList.add(1);
     setState(() {
       listofwidgets = tempList;
     });
   }
 
-  void showmyTile() {}
+  cleartextfield() {
+    _c.clear();
+  }
 
-  Future<void> _showTile() async {
-    return showmyTile<void>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            contentPadding: const EdgeInsets.all(16.0),
-            content: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                          labelText: "Text", hintText: "Input Ideas Here")),
-                )
-              ],
-            ),
-          );
-        });
+  Widget _buildPopupDialog(BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.all(16.0),
+      content: Row(
+        children: [
+          Expanded(
+            child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    labelText: "Text", hintText: "Input Ideas Here"),
+                controller: _c),
+          ),
+          SizedBox(height: 10.0),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  if (i == 0) {
+                    this._text[i] = _c.text;
+                  } else {
+                    this._text.add(_c.text);
+                  }
+                });
+                i = i + 1;
+                cleartextfield();
+                Navigator.pop(context);
+              },
+              child: Text("Save"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _c = TextEditingController();
+
+    super.initState();
   }
 
   @override
@@ -51,18 +76,62 @@ class _IdeasState extends State<Ideas> {
               itemCount: listofwidgets.length,
               itemBuilder: (context, index) {
                 return Container(
-                  height: 100.0,
+                  // height: 200.0,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                         vertical: 10.0, horizontal: 10.0),
-                    child: Card(
-                      child: ListTile(
-                          onTap: () {
-                            _showTile;
-                          },
-                          title: Text('Ideas Please !!')),
-                      color: Colors.grey[100],
-                      margin: EdgeInsets.all(1.0),
+                    child: SizedBox(
+                      height: 100,
+                      child: Card(
+                        elevation: 10.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0)),
+                        child: ListTile(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/milestones');
+                            },
+                            onLongPress: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    _buildPopupDialog(context),
+                              );
+                            },
+                            title: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                _text[index],
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            subtitle: Column(
+                              children: [
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      TextButton(
+                                          onPressed: () {},
+                                          child: Icon(
+                                            Icons.delete,
+                                            color: Colors.grey[700],
+                                          )),
+                                      TextButton(
+                                          onPressed: () {},
+                                          child: Icon(
+                                            Icons.edit,
+                                            color: Colors.grey[700],
+                                          ))
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )),
+                        color: Colors.grey[300],
+                        margin: EdgeInsets.all(5.0),
+                      ),
                     ),
                   ),
                 );
@@ -70,6 +139,10 @@ class _IdeasState extends State<Ideas> {
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             additemtolist();
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialog(context),
+            );
           },
           child: const Icon(Icons.add),
           backgroundColor: Colors.amber[600]),
--- lib/screens/milestones.dart
+++ lib/screens/milestones.dart
@@ -6,10 +6,103 @@ class Milestones extends StatefulWidget {
 }
 
 class _MilestonesState extends State<Milestones> {
+  List<int> listcount = [];
+  List<bool> _status = [];
+  List<String> _text1 = ['Add Milestones'];
+  TextEditingController _c1;
+
+  var j = 0;
+
+  void createList() {
+    List<int> tempL = listcount;
+    tempL.add(1);
+    setState(() {
+      listcount = tempL;
+    });
+  }
+
+  Widget _buildPopup1Dialog(BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.all(16.0),
+      content: Row(
+        children: [
+          Expanded(
+            child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    labelText: "Text", hintText: "Input Milestones Here"),
+                controller: _c1),
+          ),
+          SizedBox(height: 10.0),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  if (j == 0) {
+                    this._text1[j] = _c1.text;
+                  } else {
+                    this._text1.add(_c1.text);
+                  }
+                });
+                j = j + 1;
+                Navigator.pop(context);
+              },
+              child: Text("Save"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _c1 = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Milestones"),
+        centerTitle: true,
+        backgroundColor: Colors.amber[600],
+      ),
+      body: Container(
+          child: ListView.builder(
+        itemCount: listcount.length,
+        itemBuilder: (context, index) {
+          return Container(
+              child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Card(
+              child: CheckboxListTile(
+                value: _status[index],
+                title: Text(_text1[index]),
+                onChanged: (bool check) {
+                  setState(() {
+                    _status[index] = check;
+                  });
+                },
+              ),
+              margin: EdgeInsets.all(1.0),
+              color: Colors.grey[100],
+            ),
+          ));
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          createList();
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopup1Dialog(context));
+
+          setState(() {
+            _status = List<bool>.filled(listcount.length, false);
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber[600],
+      ),
     );
   }
 }
